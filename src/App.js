273d0dba960c3e427366b675c943b1555d8fff16
inadislam <inadislam@gmail.com>
2020-05-06 03:07:56 +0600

    Confirmed Cases, Recoveries, Deaths Counter Added

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from './api';
+
+import styles from './App.module.css';
+
+class App extends React.Component {
+
+    state = {
+        data: {},
+    }
+    
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+
+        this.setState({ data: fetchedData })
+    }
+
+    render(){
+        return(
+            <div className={styles.container}>
+                <Cards data={this.state.data} />
+                <CountryPicker />
+                <Chart />
+            </div>
+        );
+    }
+}
+
+export default App;
