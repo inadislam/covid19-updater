5b58a59d33d409d0274ecd52a13942e81a27aba5
inadislam <inadislam@gmail.com>
2020-05-09 11:48:10 +0600

    Added some changes

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardContent, Typography, Grid } from '@material-ui/core';
 import CountUp from 'react-countup';
 import cx from 'classnames';
+//import reactStringReplace from 'react-string-replace';
 
 import styles from './Cards.module.css';
 
@@ -9,6 +10,26 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
     if(!confirmed) {
         return 'Loading...';
     }
+
+    /*const engToBn = (string) => {
+        let engNumb = {
+            '0': '০',
+            '1': '১',
+            '2': '২',
+            '3': '৩',
+            '4': '৪',
+            '5': '৫',
+            '6': '৬',
+            '7': '৭',
+            '8': '৮',
+            '9': '৯'
+        }
+        for (var x in engNumb) {
+            string = string.replace(new RegExp(x, 'g'), engNumb[x]);
+        }
+        return string;
+    }*/
+
     return(
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
@@ -30,7 +51,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                 </Grid>
                 <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5"><CountUp start={0} end={deaths.value} duration={2.5} separator="," /></Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Numbers of People Deaths Caused by Covid-19</Typography>
